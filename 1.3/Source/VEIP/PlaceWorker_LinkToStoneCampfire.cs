001aa546ba331985fa8ad2cc23dac6c5a48f6be3
Kylian <kylian.balde@gmail.com>
2021-08-27 18:30:45 +0200

    Fix, VFE Art, Vikings, VFE

---
+++ 1.3/Source/VEIP/PlaceWorker_LinkToStoneCampfire.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace VEIP
+{
+    public class PlaceWorker_LinkToStoneCampfire : PlaceWorker
+    {
+        public float range = 11.9f;
+
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
+        {
+            GenDraw.DrawRadiusRing(center, this.range);
+            List<Thing> forCell = Find.CurrentMap.listerBuldingOfDefInProximity.GetForCell(center, this.range, VEIP_DefOf.Stone_Campfire);
+            for (int index = 0; index < forCell.Count; ++index)
+                GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), forCell[index].TrueCenter(), SimpleColor.Green);
+        }
+    }
+}
---
+++ 1.3/Source/VEIP/VEIP_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VEIP
+{
+    [DefOf]
+    internal class VEIP_DefOf
+    {
+        public static ThingDef Stone_Campfire;
+    }
+}
