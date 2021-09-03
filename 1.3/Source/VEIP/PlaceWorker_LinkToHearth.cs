4f35dfd2892f7bfef9d13d65385b007956ad3d4e
Kylian <kylian.balde@gmail.com>
2021-09-03 09:11:25 +0200

    Vikings heart can be used as ritual spot and for drum party

---
+++ 1.3/Source/VEIP/PlaceWorker_LinkToHearth.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace VEIP
+{
+    class PlaceWorker_LinkToHearth : PlaceWorker
+    {
+        public float range = 11.9f;
+
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
+        {
+            GenDraw.DrawRadiusRing(center, this.range);
+            ThingDef campfire = DefDatabase<ThingDef>.GetNamedSilentFail("VFEV_Hearth");
+            if (campfire != null)
+            {
+                List<Thing> forCell = Find.CurrentMap.listerBuldingOfDefInProximity.GetForCell(center, this.range, campfire);
+                for (int index = 0; index < forCell.Count; ++index)
+                    GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), forCell[index].TrueCenter(), SimpleColor.Green);
+            }
+        }
+    }
+}
